6aa86d6875d09404551ee198655e3e2cdddf1bd9
Adyagoenka18 <72975435+Adyagoenka18@users.noreply.github.com>
2021-02-27 14:05:38 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,24 @@
+class BlockClass{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("sprites/base.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+       block1;block2;block3;block4;block4;block5;block6;block7
+}
